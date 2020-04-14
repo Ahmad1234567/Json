f4d34b47f9cbe8695cd7077297e2b59fdb1f493d
Muhammad Ahmed <mahmad.comsats@gmail.com>
2020-04-14 13:49:02 +0500

    Initial commit

---
+++ app/src/main/java/com/example/json/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.json;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+  //  public static final String JSON_STRING="{\"employee\":{\"name\":\"Ali\",\"salary\":56000}}";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView1=(TextView)findViewById(R.id.textView1);
+
+        try{
+            JSONObject emp= new JSONObject("{\"employee\":{\"name\":\"Ali\",\"salary\":56000}}").getJSONObject("employee");
+            String empname=emp.getString("name");
+            int empsalary=emp.getInt("salary");
+
+            String str="Employee Name:"+empname+"\n"+"Employee Salary:"+empsalary;
+            textView1.setText(str);
+
+        }catch (Exception e) {e.printStackTrace();}
+
+    }
+
+}
